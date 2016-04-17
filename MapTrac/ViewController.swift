d2f9d66373c6b911c42412a6d883a971a1648996
mrrboto <mrrboto@gmail.com>
2016-04-16 19:18:59 -0700

    Initial commit

---
+++ MapTrac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MapTrac
+//
+//  Created by user on 4/13/16.
+//  Copyright © 2016 VAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "grad_green.jpg")!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
